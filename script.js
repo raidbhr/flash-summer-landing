85c364f0aafbd3fed137f9c8be6a9fba2a65f16b
raidbhr <raidbhr2@gmail.com>
2026-07-23 10:33:32 +0100

    Update GOOGLE_SCRIPT_URL to new deployment ID

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // ⚠️ عدّل هذا الرابط برابط Google Apps Script Web App الخاص بك
 // اشرح طريقة الحصول عليه بالتفصيل في ملف README.md
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw5QE-n84fX-5csmlcNy5O8DR6Z211mZo4pBf9vbuWu2zj63lchV4FzqRMrp4NkswDA/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHeaderReveal();
